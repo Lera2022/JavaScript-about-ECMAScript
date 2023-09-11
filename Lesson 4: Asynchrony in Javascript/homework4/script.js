6c59194163af63db0308e67c2b3a68dc13b04655
Lera <valeriiakolesnikova21@gmail.com>
2023-09-11 13:16:37 +0500

    homework 4 added

--- Lesson 4: Asynchrony in Javascript/homework4/script.js
+++ Lesson 4: Asynchrony in Javascript/homework4/script.js
@@ -1,27 +1,36 @@
-/* https://api.nasa.gov/ */
+/* https://dog.ceo/dog-api/ */
 
-async function myAsync(url) {
-	const response = await fetch(url);
-	const json = await response.json();
-	return json;
-}
+
 const body = document.querySelector('body');
 const url = 'https://dog.ceo/api/breeds/image/random';
 
-try {
-	const myData = await myAsync(url);
-	console.log(myData);
-	myData.forEach((el) => {
+
+const getData2 = async (url) => {
+	const res = await fetch(url);
+	const data = await res.json();
+	return data;
+};
+
+async function addDog() {
+	try {
+		const data = await getData2(url);
 		body.insertAdjacentHTML(
 			'beforeend',
 			`
-				<figure>
-					<img src="${el.message}" />
-					<figcaption>${el.status}</figcaption>
-				</figure>
-  		`
+			<figure>
+			<img src="${data.message}" />
+			</figure>
+			`
 		);
-	});
-} catch (error) {
-	console.log(error.message);
+	} catch (error) {
+		console.log('Что-то пошло не так');
+	}
 }
+
+let timerId = setInterval(function () {
+	addDog();
+}, 3000);
+setTimeout(() => {
+	clearInterval(timerId);
+	addDog();
+}, 27000);
